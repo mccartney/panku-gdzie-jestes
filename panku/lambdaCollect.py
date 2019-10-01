430a705e2e1852199381f40a09a7e1436b46f282
Grzegorz Olędzki <grzegon@poczta.onet.pl>
2019-10-01 14:32:54 +0200

    Adding Veturilo (still slow :/)

--- panku/lambdaCollect.py
+++ panku/lambdaCollect.py
@@ -4,6 +4,7 @@ import requests
 import boto3
 import time
 import geopy.distance
+import xml.etree.ElementTree as ET
 
 # special artificial timestamp value for keeping the last known location
 LATEST = -1
@@ -22,31 +23,6 @@ class Service(object):
     get_secret_value_response = client.get_secret_value(SecretId=secret_name)
     return get_secret_value_response["SecretString"].replace('"', '').replace("{","").replace("}", "").split(":")
 
-  def getLocations(self):
-    s = requests.Session()
-    s.headers.update({"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:64.0) Gecko/20100101 Firefox/64.0",
-                      "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-                      "Accept-Language": "en-GB,en;q=0.7,en-US;q=0.3",
-                      "Upgrade-Insecure-Requests": "1",
-                      "DNT": "1",
-                      })
-    s.get("https://panel.panekcs.pl/security/login")
-
-    username, password = self.getCredentials()
-    r = s.post(url = "https://panel.panekcs.pl/security/login", data={"UserName": username, "Password": password})
-    assert r.status_code == 200
-
-    r = s.post(url = "https://panel.panekcs.pl/Home/GetLocations", data = {})
-    assert r.status_code == 200
-    locations = r.json()
-
-    # Under Vehicles: [u'Category', u'FuelRange', u'Ids', u'Coordinates', u'RegistrationNumber', u'Fuel']
-    count = len(locations['Vehicles']['Ids'])
-
-    coordinates = locations['Vehicles']['Coordinates']
-    registrations = locations['Vehicles']['RegistrationNumber']
-
-    return zip(registrations, coordinates)
 
   def keyPrefix(self):
     pass
@@ -80,7 +56,7 @@ class Service(object):
          if existedBefore:
            print("storing previous")
            r = table.put_item(Item = {'carId' : key, 'date' : now-1,'long': prevPosition[0], 'lat': prevPosition[1]})
-           
+
          r = table.put_item(Item = {'carId' : key, 'date' : now,    'long': "%8.6f" % position['lng'], 'lat': "%8.6f" % position['lat']})
          r = table.put_item(Item = {'carId' : key, 'date' : LATEST, 'long': "%8.6f" % position['lng'], 'lat': "%8.6f" % position['lat']})
 
@@ -92,7 +68,57 @@ class Panek(Service):
     return "panek/login"
   def keyPrefix(self):
     return "PANEK"
+  def getLocations(self):
+    s = requests.Session()
+    s.headers.update({"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:64.0) Gecko/20100101 Firefox/64.0",
+                      "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+                      "Accept-Language": "en-GB,en;q=0.7,en-US;q=0.3",
+                      "Upgrade-Insecure-Requests": "1",
+                      "DNT": "1",
+                      })
+    s.get("https://panel.panekcs.pl/security/login")
+
+    username, password = self.getCredentials()
+    r = s.post(url = "https://panel.panekcs.pl/security/login", data={"UserName": username, "Password": password})
+    assert r.status_code == 200
+
+    r = s.post(url = "https://panel.panekcs.pl/Home/GetLocations", data = {})
+    assert r.status_code == 200
+    locations = r.json()
+
+    # Under Vehicles: [u'Category', u'FuelRange', u'Ids', u'Coordinates', u'RegistrationNumber', u'Fuel']
+    count = len(locations['Vehicles']['Ids'])
+
+    coordinates = locations['Vehicles']['Coordinates']
+    registrations = locations['Vehicles']['RegistrationNumber']
+
+    return zip(registrations, coordinates)
+
+class Veturilo(Service):
+  def keyPrefix(self):
+    return "VETURILO"
+  def getLocations(self):
+    s = requests.Session()
+    s.headers.update({"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:64.0) Gecko/20100101 Firefox/64.0",
+                      "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+                      "Accept-Language": "en-GB,en;q=0.7,en-US;q=0.3",
+                      "Upgrade-Insecure-Requests": "1",
+                      "DNT": "1",
+                      })
+    r = s.get("https://nextbike.net/maps/nextbike-official.xml?city=372,210,475")
+    assert r.status_code == 200
+    
+    root = ET.fromstring(r.content)
+    
+    ret = []
+    for place in root.findall(".//place"):
+      for bike in place:
+        ret.append((bike.get("number"), {"lng" : float(place.get("lng")), "lat": float(place.get("lat"))}))
+    
+    return ret
+     
 
 def lambda_handler(event, context):
+  Veturilo().getAndSaveLocations()
   Panek().getAndSaveLocations()
   return "OK"
